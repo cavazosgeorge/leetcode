5183c8720062100f4653b7d34fe702aa48bb5fed
George Cavazos <65411983+cavazosgeorge@users.noreply.github.com>
2022-06-15 00:07:34 -0400

    Time: 86 ms (48.03%), Space: 42.7 MB (21.75%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+var isValid = function (s) {
+  let stack = []
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
+      stack.push(s[i])
+    } else {
+      if (stack.length === 0) {
+        return false
+      }
+      let top = stack.pop()
+      if (top === "(" && s[i] !== ")") {
+        return false
+      }
+      if (top === "[" && s[i] !== "]") {
+        return false
+      }
+      if (top === "{" && s[i] !== "}") {
+        return false
+      }
+    }
+  }
+  return stack.length === 0
+}
