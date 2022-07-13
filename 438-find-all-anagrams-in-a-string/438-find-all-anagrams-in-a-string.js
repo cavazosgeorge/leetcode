5db7a5dd73456a5e41385aa2ec3a05b2a047c15f
George Cavazos <65411983+cavazosgeorge@users.noreply.github.com>
2022-07-13 12:17:48 -0400

    Time: 267 ms (31.62%), Space: 49.8 MB (27.69%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.js
@@ -0,0 +1,49 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {number[]}
+ */
+var findAnagrams = function(s, p) {
+  let windowStart = 0,
+    matched = 0,
+    charFrequency = {};
+
+  for (i = 0; i < p.length; i++) {
+    const chr = p[i];
+    if (!(chr in charFrequency)) {
+      charFrequency[chr] = 0;
+    }
+    charFrequency[chr] += 1;
+  }
+
+  const resultIndices = [];
+    
+  for (windowEnd = 0; windowEnd < s.length; windowEnd++) {
+    const rightChar = s[windowEnd];
+    if (rightChar in charFrequency) {
+      // decrement the frequency of matched character
+      charFrequency[rightChar] -= 1;
+      if (charFrequency[rightChar] === 0) {
+        matched += 1;
+      }
+    }
+
+    if (matched === Object.keys(charFrequency).length) { 
+      resultIndices.push(windowStart);
+    }
+
+    // shrink the sliding window
+    if (windowEnd >= p.length - 1) {
+      leftChar = s[windowStart];
+      windowStart += 1;
+      if (leftChar in charFrequency) {
+        if (charFrequency[leftChar] === 0) {
+          matched -= 1; // before putting the character back, decrement the matched count
+        }
+        charFrequency[leftChar] += 1; // put the character back
+      }
+    }
+  }
+
+  return resultIndices;
+}
