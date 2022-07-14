c5d996935103059f9b08b4143de2d5fd25d7d70a
George Cavazos <65411983+cavazosgeorge@users.noreply.github.com>
2022-07-14 05:23:35 -0400

    Time: 132 ms (8.38%), Space: 42.6 MB (86.05%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -7,9 +7,9 @@ var twoSum = function(numbers, target) {
     const nums = {};
     for (let i = 0; i < numbers.length; i++) {
         let num = numbers[i];
-        let seek = target - num;
-        if (seek in nums) {
-            return [nums[seek] - 1, i + 1];
+        let potentialMatch = target - num;
+        if (potentialMatch in nums) {
+            return [nums[potentialMatch] - 1, i + 1];
         }
         
         nums[num] = i + 2;
