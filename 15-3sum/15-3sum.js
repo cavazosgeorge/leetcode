ebb317b61ee7b8aea7fcf2b6d855732bfb6956ca
George Cavazos <65411983+cavazosgeorge@users.noreply.github.com>
2022-07-16 04:45:21 -0400

    Time: 146 ms (92.04%), Space: 52.2 MB (81.13%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -2,34 +2,39 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    const triplets = [];
+    for (let i = 0; i < nums.length; i++) {
+        // SKIP SAME ELEMENT TO AVOID DUPLICATE TRIPLETS
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
 
-var threeSum = function (nums) {
-  let result = []
-  nums.sort((a, b) => a - b)
-  for (let i = 0; i < nums.length - 2; i++) {
-    if (i > 0 && nums[i] === nums[i - 1]) {
-      continue
+        searchPair(nums, -nums[i], i + 1, triplets);
     }
-    let left = i + 1
-    let right = nums.length - 1
+
+    return triplets;
+}
+
+const searchPair = function(nums, targetSum, left, triplets) {
+    let right = nums.length - 1; // END INDEX
     while (left < right) {
-      let sum = nums[i] + nums[left] + nums[right]
-      if (sum === 0) {
-        result.push([nums[i], nums[left], nums[right]])
-        while (left < right && nums[left] === nums[left + 1]) {
-          left++
+        const currentSum = nums[left] + nums[right] 
+        if (currentSum === targetSum) { // FOUND THE TRIPLET
+            triplets.push([-targetSum, nums[left], nums[right]]);
+            left++;
+            right--;
+            while (left < right && nums[left] === nums[left - 1]) { 
+                left++; //SKIP SAME ELEMENT TO AVOID DUPLICATE TRIPLETS
+            }
+            while (left < right && nums[right] === nums[right + 1]) {
+                right--; // SKIP SAME ELEMENT TO AVOID DUPLICATE TRIPLETS
+            }
+        } else if (targetSum > currentSum) {
+            left++; // NEED PAIR WITH BIGGER SUM
+        } else {
+            right--; // NEED PAIR WITH SMALLER SUM
         }
-        while (left < right && nums[right] === nums[right - 1]) {
-          right--
-        }
-        left++
-        right--
-      } else if (sum > 0) {
-        right--
-      } else {
-        left++
-      }
     }
-  }
-  return result
 }
