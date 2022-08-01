e40725a3358bdf6de55205f2691ec9178e241ae0
George Cavazos <65411983+cavazosgeorge@users.noreply.github.com>
2022-07-31 23:16:24 -0400

    Time: 93 ms (78.30%), Space: 42.5 MB (67.84%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,15 +4,16 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  const numsMap = {}; // to store numbers and their indices
-  for (let i = 0; i < nums.length; i++) {
-    const num = nums[i];
-    if (target - num in numsMap) {
-      return [numsMap[target - num], i];
-    }
-      
-    numsMap[nums[i]] = i;
-  }
+    const set = {};
     
-  return [];
-}
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        if (target - num in set) {
+            return [set[target - num], i];
+        }
+        
+        set[nums[i]] = i;
+    }
+    
+    return [-1, -1];
+};
