8f7d2131a75bb927e8a9ce25e0deeb78694fc1d5
George Cavazos <65411983+cavazosgeorge@users.noreply.github.com>
2022-08-05 05:20:56 -0400

    Time: 115 ms (58.93%), Space: 43.9 MB (98.50%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -4,11 +4,12 @@
  */
 var removeDuplicates = function(nums) {
     let slow = 0;
-    for (let fast = 0; fast < nums.length; fast++) {
+    for (let fast = 1; fast < nums.length; fast++) {
         if (nums[fast] !== nums[slow]) {
             slow++;
             nums[slow] = nums[fast];
         }
     }
+    
     return slow + 1;
 };
