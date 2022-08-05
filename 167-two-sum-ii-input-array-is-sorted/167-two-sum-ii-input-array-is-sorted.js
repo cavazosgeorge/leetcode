5b0e9ca4aa53f8d1c84e85da0a63f5b768d994bb
George Cavazos <65411983+cavazosgeorge@users.noreply.github.com>
2022-08-05 11:01:45 -0400

    Time: 93 ms (54.11%), Space: 43.1 MB (33.88%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -3,17 +3,33 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(numbers, target) {
-    const nums = {};
-    for (let i = 0; i < numbers.length; i++) {
-        let num = numbers[i];
-        let potentialMatch = target - num;
-        if (potentialMatch in nums) {
-            return [nums[potentialMatch] - 1, i + 1];
-        }
+// var twoSum = function(numbers, target) {
+//     const nums = {};
+//     for (let i = 0; i < numbers.length; i++) {
+//         let num = numbers[i];
+//         let potentialMatch = target - num;
+//         if (potentialMatch in nums) {
+//             return [nums[potentialMatch] - 1, i + 1];
+//         }
         
-        nums[num] = i + 2;
-    }
+//         nums[num] = i + 2;
+//     }
     
+//     return [];
+// };
+
+var twoSum = function(numbers, target) {
+    let left = 0, right = numbers.length - 1;
+    while (left < right) {
+        const currentSum = numbers[left] + numbers[right];
+        if (currentSum === target) {
+            return [left + 1, right + 1];
+        } 
+        if (currentSum < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
     return [];
-};
+}
