776699b4f046403134944ca59b7e2a9e16660934
George Cavazos <65411983+cavazosgeorge@users.noreply.github.com>
2022-08-06 00:23:03 -0400

    Time: 110 ms (52.02%), Space: 49.4 MB (15.83%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -2,20 +2,44 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
-    
-    // turn string to lowercase and use regex to remove non-alphanumeric
-    s = s.toLowerCase();
-    s = s.replace(/[^A-Za-z0-9]/g, '');
 
-    let start = 0;
-    let end = s.length-1; 
+// var isPalindrome = function(s) {
     
-    while (start < end){
+//     // turn string to lowercase and use regex to remove non-alphanumeric
+//     s = s.toLowerCase();
+//     s = s.replace(/[^A-Za-z0-9]/g, '');
+
+//     let start = 0;
+//     let end = s.length-1; 
+    
+//     while (start < end){
         
-        if(s[start] !== s[end]) return false
-        start++;
-        end--;
+//         if(s[start] !== s[end]) return false
+//         start++;
+//         end--;
+//     }
+//     return true
+// };
+
+
+function isAlphaNumeric(c) {
+    return /^[a-zA-Z0-9]*$/.test(c);
+}
+
+function isPalindrome(s) {
+    let l = 0, r = s.length - 1;
+    while (l < r) {
+        while (l < r && !isAlphaNumeric(s.charAt(l))) {  // Note 1, 2
+            l++;
+        }
+        while (l < r && !isAlphaNumeric(s.charAt(r))) {
+            r--;
+        }
+        if (s.charAt(l).toLowerCase() !== s.charAt(r).toLowerCase()) {  // ignore case
+            return false;
+        }
+        l++;
+        r--;
     }
-    return true
-};
+    return true;
+}
