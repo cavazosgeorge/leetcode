891da13e93b644dda817cd03cec0d50265287654
George Cavazos <65411983+cavazosgeorge@users.noreply.github.com>
2022-08-06 03:42:37 -0400

    Time: 131 ms (66.58%), Space: 47 MB (72.11%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -20,23 +20,40 @@
 //   return answer
 // }
 
-var lengthOfLongestSubstring = function (s) {
-    let n = s.length, end = 0;
-    let inside = new Set(s[0]); // CONTAINS CHARACTERS IN [START, END]
-    let answer = 0;
+// var lengthOfLongestSubstring = function (s) {
+//     let n = s.length, end = 0;
+//     let inside = new Set(s[0]); // CONTAINS CHARACTERS IN [START, END]
+//     let answer = 0;
     
-    console.log('S', s);
-    console.log('Set', inside);
+//     console.log('S', s);
+//     console.log('Set', inside);
     
-    for (let start = 0; start < n; start++) {
-        while (end + 1 < n && !inside.has(s[end + 1])) {
-            end++;
-            inside.add(s[end])
-        } 
+//     for (let start = 0; start < n; start++) {
+//         while (end + 1 < n && !inside.has(s[end + 1])) {
+//             end++;
+//             inside.add(s[end])
+//         } 
         
-        answer = Math.max(answer, end - start + 1);
-        inside.delete(s[start]);
-    }
+//         answer = Math.max(answer, end - start + 1);
+//         inside.delete(s[start]);
+//     }
     
-    return answer
+//     return answer
+// }
+
+var lengthOfLongestSubstring = function(s) {
+    let n = s.length, longest = 0, left = right = 0;
+    const window = new Set();
+    while (right < n) {
+        if (!window.has(s.charAt(right))) {
+            window.add(s.charAt(right));
+            right++;
+        } else {
+            window.delete(s.charAt(left));
+            left++;
+        }
+        longest = Math.max(longest, right - left);
+    }
+    return longest
 }
+
