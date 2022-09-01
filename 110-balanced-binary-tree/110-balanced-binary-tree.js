784e9ed56bb71bd14e647b0c7dad000e8fb82d34
George Cavazos <65411983+cavazosgeorge@users.noreply.github.com>
2022-09-01 05:09:42 -0400

    Time: 133 ms (22.98%), Space: 46.9 MB (82.28%) - LeetHub

---
+++ 110-balanced-binary-tree/110-balanced-binary-tree.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    return treeHeight(root) !== -1;
+};
+
+const treeHeight = (root) => {
+    if (!root) return 0;
+    const leftHeight = treeHeight(root.left);
+    const rightHeight = treeHeight(root.right);
+    
+    if (leftHeight === -1 || rightHeight === -1) return -1;
+    if (Math.abs(leftHeight - rightHeight) > 1) return -1;
+    return Math.max(leftHeight, rightHeight) + 1;
+}
+
