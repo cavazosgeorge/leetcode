17c3d7e1f79aa39d93b2ce71841082a3fea5b38e
George Cavazos <65411983+cavazosgeorge@users.noreply.github.com>
2022-09-03 02:31:19 -0400

    Time: 114 ms (14.92%), Space: 42.6 MB (26.78%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -2,11 +2,16 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var runningSum = function(nums) {
-    let total = 0;
-    return nums.map((num) => total += num)
-};
+// var runningSum = function(nums) {
+//     let total = 0;
+//     return nums.map((num) => total += num)
+// };
 
 
-
-
+const runningSum = (nums) => {
+    for (let i = 1; i < nums.length; i++) {
+        nums[i] = nums[i] + nums[i - 1];
+    }
+    
+    return nums;
+}
