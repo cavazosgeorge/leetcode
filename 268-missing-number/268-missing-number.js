ece9cf5b547c4d86ae01ea8b5ba7b4ab0294f72d
George Cavazos <65411983+cavazosgeorge@users.noreply.github.com>
2022-09-15 11:41:10 -0400

    Time: 129 ms (32.80%), Space: 44.4 MB (62.13%) - LeetHub

--- 268-missing-number/268-missing-number.js
+++ 268-missing-number/268-missing-number.js
@@ -2,12 +2,37 @@
  * @param {number[]} nums
  * @return {number}
  */
-const missingNumber = (nums) => {
-    let result = new Array(nums.length + 1).fill(-1);
+// const missingNumber = (nums) => {
+//     let result = new Array(nums.length + 1).fill(-1);
     
-    for (const num of nums) {
-        result[num] = num;
+//     for (const num of nums) {
+//         result[num] = num;
+//     }
+    
+//     return result.indexOf(-1);
+// }
+
+
+// solution #2
+const missingNumber = (nums) => {
+    let i = 0, j = null;
+    const n = nums.length;
+    
+    while (i < nums.length) {
+        j = nums[i];
+        if (nums[i] < n && nums[i] !== nums[j]) {
+            [nums[i], nums[j]] = [nums[j], nums[i]]; // perform swap
+        } else {
+            i += 1;
+        }
     }
     
-    return result.indexOf(-1);
+    // find the first number missing from its index, that will be our number
+    for (let i = 0; i < n; i++) {
+        if (nums[i] !== i) {
+            return i;
+        }
+    }
+    
+    return n;
 }
