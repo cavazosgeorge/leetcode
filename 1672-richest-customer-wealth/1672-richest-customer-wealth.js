607388956d50a080873d1dbad72f9e3a69a51cd0
George Cavazos <65411983+cavazosgeorge@users.noreply.github.com>
2022-09-21 15:38:40 -0400

    Time: 121 ms (5.86%), Space: 42 MB (84.61%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.js
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.js
@@ -2,18 +2,17 @@
  * @param {number[][]} accounts
  * @return {number}
  */
+const maximumWealth = function (accounts) {
+  let maxWealthSoFar = 0
 
-const maximumWealth = (accounts) => {
-    let maxWealthSoFar = 0;
-    
-    for (let i = 0; i < accounts.length; i++) {
-        let currentCustomerWealth = 0;
-        for (let j = 0; j < accounts[i].length; j++) {
-            currentCustomerWealth += accounts[i][j];
-        }
-        
-        maxWealthSoFar = Math.max(maxWealthSoFar, currentCustomerWealth)
+  for (let row = 0; row < accounts.length; ++row) {
+    let currentCustomerWealth = 0
+    for (let col = 0; col < accounts[row].length; ++col) {
+      currentCustomerWealth += accounts[row][col]
     }
-    
-    return maxWealthSoFar;
+
+    maxWealthSoFar = Math.max(maxWealthSoFar, currentCustomerWealth)
+  }
+
+  return maxWealthSoFar
 }
